5988c091246ced1d6ff9bc8fc84a9f79fb103306
mmvergara <mark.jesusmanabat@gmail.com>
2023-09-06 12:53:24 +0800

    refactor cloudinary driver

--- Backend/src/cloudinary/cloudinary.ts
+++ Backend/src/cloudinary/cloudinary.ts
@@ -5,8 +5,10 @@ import {
   CLOUDINARY_CLOUD_NAME,
 } from "../config";
 
-export const Cloudinary = cloudinary.config({
+cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
   api_key: CLOUDINARY_API_KEY,
   api_secret: CLOUDINARY_API_SECRET,
 });
+
+export const Cloudinary = cloudinary;
