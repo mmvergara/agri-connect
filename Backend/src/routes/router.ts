149712e1cb3d1f51fe1efec6ded7bdf0912a66fb
mmvergara <mark.jesusmanabat@gmail.com>
2023-09-06 12:53:32 +0800

    added product routes

--- Backend/src/routes/router.ts
+++ Backend/src/routes/router.ts
@@ -1,10 +1,12 @@
 import authenticationRoutes from "./auth-routes";
 import type { Router } from "express";
 import express from "express";
+import productRoutes from "./product-routes";
 
 const router = express.Router();
 
 export default (): Router => {
   authenticationRoutes(router);
+  productRoutes(router);
   return router;
 };
